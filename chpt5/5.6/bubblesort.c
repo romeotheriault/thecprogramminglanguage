2266f5212c63c3e7eeaa90ba34a4912ae93a3f91
Romeo Theriault <romeotheriault@gmail.com>
2021-09-18 21:15:02 -1000

    add bubblesort performance timing.

--- chpt5/5.6/bubblesort.c
+++ chpt5/5.6/bubblesort.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
-#include <string.h>
+#include <sys/random.h>
+
+/*
+// Array of 20k elements
+real    0m0.622s
+user    0m0.578s
+sys     0m0.000s
+
+// Array of 200k elements
+real    2m38.689s
+user    2m37.047s
+sys     0m0.313s
+*/
+
 
 void bubblesort(int *a, int alen)
 {
@@ -23,11 +36,14 @@ void printarray (int *a, int len)
 
 int main (void)
 {
-    int a[] = {9,8,10,4,3,7,6,5,1};
-    printf("unsorted array: ");
+    const int MAXLEN = 200000;
+    int a[MAXLEN];
+    ssize_t rc = getrandom(a, MAXLEN, 0);
+    //printf("unsorted array: ");
     int alen = sizeof(a)/sizeof(*a);
-    printarray(a, alen);
+    //printarray(a, alen);
     bubblesort(a, alen);
     printf("sorted array: ");
-    printarray(a, alen);
+    //printarray(a, alen);
+    printf("last element: %d\n", a[MAXLEN-1]);
 }
