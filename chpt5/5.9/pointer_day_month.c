05955a7b752c6ac31c882af6cc1485917225bd78
Romeo Theriault <romeotheriault@gmail.com>
2021-09-23 21:18:17 -1000

    5.9 - pointer day of month

--- chpt5/5.9/pointer_day_month.c
+++ chpt5/5.9/pointer_day_month.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-static char daytab[2][13] = {
-    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
-};
+char nonleapyear[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+char leapyear[] =    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+static char *daytab[2] = {nonleapyear, leapyear};
 
 /* day_of_year: set day of year from month & day */
 int day_of_year(int year, int month, int day)
@@ -16,8 +15,12 @@ int day_of_year(int year, int month, int day)
     }
 
     leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
-    for (i = 1; i < month; i++)
-        day += daytab[leap][i];
+    char *origAddr = daytab[leap];
+    while (month-- > 0)
+        day += *daytab[leap]++;
+
+    // Set back to beginning of the array
+    daytab[leap] = ++origAddr;
     return day;
 }
 
@@ -34,8 +37,11 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
         return;
     }
 
-    for (i = 1; yearday > daytab[leap][i]; i++)
-        yearday -= daytab[leap][i];
+    i = 1;
+    while (yearday > *(++daytab[leap]) && i++) {
+        yearday -= *(daytab[leap]);
+    }
+
     *pmonth = i;
     *pday = yearday;
 }
