4c71bd4309dbe151465c6ae483d12977e05b0bd8
Romeo Theriault <romeotheriault@gmail.com>
2021-09-28 22:13:18 -1000

    start of tail

---
+++ chpt5/5.13/tail.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Write the program tail which prints the last n lines of it's input.
+   By default n is 10, by can be changed by an optional argument
+
+   tail -n 20
+
+   The program should behave rationally no matter how unreasonable the input 
+   or the value of n.
+   Write the program so it makes the best use of available storage. 
+   Lines should be stored as in the sorting program of section 5.6. i.e. Not a
+   two dimensional array of fixed size.
+
+*/
+
