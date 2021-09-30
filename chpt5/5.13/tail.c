858f209e6e84f862223e17f070a3b5b220295e54
Romeo Theriault <romeotheriault@gmail.com>
2021-09-29 22:45:08 -1000

    5.13 - tail

--- chpt5/5.13/tail.c
+++ chpt5/5.13/tail.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /* Write the program tail which prints the last n lines of it's input.
    By default n is 10, by can be changed by an optional argument
@@ -13,3 +16,22 @@
 
 */
 
+#define MAXLINES 100000
+int main (int argc, char *argv[])
+{
+    size_t buffsize = 1000;
+    char *lines[MAXLINES];
+    int printlines = 10;
+    if (argc >= 2 && strncmp(argv[1], "-n", 2) == 0)
+        printlines = atoi(argv[2]);
+
+    size_t i;
+    for (i = 0; getline(&lines[i], &buffsize, stdin) > 0 && i < MAXLINES; i++)
+        ; 
+
+    printf("total lines: %ld\n", i);
+    printf("lines to print: %d\n", printlines);
+    
+    while (i >= printlines) 
+        printf("%s", lines[i-printlines--]);
+}
