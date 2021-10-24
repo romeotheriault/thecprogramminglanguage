2756715bef3601b31743b79e22e46c733aa5efb0
Romeo Theriault <romeotheriault@gmail.com>
2021-10-23 21:28:20 -1000

    word_count_descending - work in progress. Need to sort the array of struct pointers.

--- chpt6/6.4/word_count_descending.c
+++ chpt6/6.4/word_count_descending.c
@@ -3,11 +3,45 @@
 #include <string.h>
 #include <stdlib.h>
 
+/* 
+   Print the distinct words in it's input sorted in descending order
+   of frequency of occurence. Precede each word by it's count.
+*/
+
 #define MAXWORD 100
 #define MAXLINE 1000
 struct tnode *addtree(struct tnode *, char*);
-void treeprint(struct tnode *);
+//void treeprint(struct tnode *);
 int getword(char *, int);
+int tnode_count = 0;
+int tnode_array_index = 0;
+
+/* swap: interchange v[i] and v[j] */
+void swap(char *v[], int i, int j)
+{
+  char *temp;
+
+  temp = v[i];
+  v[i] = v[j];
+  v[j] = temp;
+}
+
+/* qsort: sort v[left]...v[right] into descending order */
+void qqsort(void *v[], int left, int right)
+{
+    int i, last;
+
+    if (right <= left) /* do nothing if the array contains */
+        return;        /* fewer than two elements */
+    swap(v, right, (left+right)/2);
+    last = right;
+    for (i = right-1; i >= left; i--)
+        if ((v[i], v[right]) < 0)
+            swap(v, --last, i);
+    swap(v, right, last);
+    qqsort(v, left, last-1);
+    qqsort(v, last+1, right);
+}
 
 /* getword: get next word or character from input */
 int getword(char *word, int lim)
@@ -56,6 +90,7 @@ struct tnode *addtree(struct tnode *t, char *word)
         t->count = 1;
         t->right = NULL;
         t->left = NULL;
+        tnode_count++;
     } 
     else if (cond = strcmp(word, t->word) == 0)
         t->count++;
@@ -66,12 +101,23 @@ struct tnode *addtree(struct tnode *t, char *word)
     return t;
 }
 
-void treeprint(struct tnode *t)
+/*void treeprint(struct tnode *t)
 {
     if (t!=NULL) {
         treeprint(t->left);
         printf("%d: %s\n", t->count, t->word);
         treeprint(t->right);
+    }
+}*/
+
+void add_node_to_array(struct tnode *t, struct tnode **tarray)
+{
+    if (t!=NULL) {
+        tnode_array_index++;
+        add_node_to_array(t->left, tarray);
+        //printf("%d: %s\n", t->count, t->word);
+        tarray[tnode_array_index] = t;
+        add_node_to_array(t->right, tarray);
     }
 }
 
@@ -86,7 +132,17 @@ int main()
         if (isalpha(word[0])) {
             root = addtree(root, word);
         }
-    treeprint(root);
+    // Allocate space for the array of tnode pointers
+    struct tnode **nodes = (struct tnode **) malloc(sizeof(struct tnode) * tnode_count);
+    add_node_to_array(root, nodes);
+    printf("tnode_array_index: %d\n", tnode_array_index);
+    printf("tnode count: %d\n", tnode_count);
+    for (int i = 1; i <= tnode_array_index; i++)
+    {
+        printf("%d: %s\n", nodes[i]->count, nodes[i]->word);
+    }
+    
+    //treeprint(root);
     return 0;
 }
 
